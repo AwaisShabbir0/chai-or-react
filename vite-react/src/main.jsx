efe7c7b85c0939a3eaf27ba95ff8155fd3e356c3
Awaiskamboh0810 <awaiskamboh0810@gmail.com>
2025-08-01 22:55:11 +0500

    custom React continue

--- vite-react/src/main.jsx
+++ vite-react/src/main.jsx
@@ -1,9 +1,30 @@
 import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyFunction() {
+  return(
+    <h1>my custom APP</h1>
+  )
+}
+const anotherElement = (
+  <a href="www.google.com" target='_blank'>Google</a>
+)
+ const anotherUser= " chai chyee??"  
+const reactElement = React.createElement(   //ALl the calculation etc done by babel behind the secenes
+  'a',
+  { href: 'https://gooogle.com', target: '_blank' },
+  'Google',
+  anotherUser
+)
+ //variable Injuction
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <StrictMode> 
+    {/* // MyFunction();  
+   // <MyFunction /> */}
+   {/* {anotherElement} */}
+    {/* <App /> */}
+    {reactElement}
+  </StrictMode>
 )
